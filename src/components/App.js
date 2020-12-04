7690251fdb70bb9ffa2f4ac28a779ec78749a3a4
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2020-12-04 13:41:55 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,18 @@ const App = () => {
   let [count, setCount] = useState(0);
 
   return (
-    <div class="ball">
+    <div className="ball">
       <h1
-        class="count"
-        ondoubleclick={() => {
+        className="count"
+        onDoubleClick={() => {
           alert("cant edit it");
         }}
       >
         {count}
       </h1>
       <button
-        class="increment-button"
-        onclick={() => {
+        className="increment-button"
+        onClick={() => {
           setCount(count + 1);
         }}
       >
